b3099258853fec5c99ec82ed0882f2052d8f187e
Marc Auberer <marc.auberer@chillibits.com>
2020-02-29 17:24:31 +0100

    Final dev version

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
     header('Location: https://mrgames13.jimdofree.com/color-converter/info');
     exit;
+
+    $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0,2);
+    $path = $_GET["p"];
+    if(!isset($path)) $path = "";
+
+    if($lang == "de") {
+        header("Location: ./de/$path");
+    } else if($lang == "fr") {
+        header("Location: ./fr/$path");
+    } else {
+        // Default language
+        header("Location: ./en/$path");
+    }
+    exit;
 ?>
