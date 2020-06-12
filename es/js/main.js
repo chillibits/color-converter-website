3c1109ebaf2f17e3c504a7d7522cc15b73f98dbb
Marc Auberer <marc.auberer@chillibits.com>
2020-06-12 15:38:21 +0200

    Update stats

--- es/js/main.js
+++ es/js/main.js
@@ -422,7 +422,7 @@ $(window).scroll(function() {
 });
 
 function animateStats() {
-  animateValue("stats1", 14400);
+  animateValue("stats1", 14500);
   animateValue("stats2", 2600);
   animateValue("stats3", 152);
   animateValue("stats4", 2700);
