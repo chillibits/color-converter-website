141477649d02efda50e935da9287a0cd0087d261
Marc Auberer <marc.auberer@sap.com>
2020-09-13 10:42:37 +0200

    Update stats

--- src/js/main.js
+++ src/js/main.js
@@ -422,7 +422,7 @@ $(window).scroll(function() {
 });
 
 function animateStats() {
-  animateValue("stats1", 22800);
+  animateValue("stats1", 22900);
   animateValue("stats2", 155);
   animateValue("stats3", 3400);
   animateValue("stats4", 3600);
