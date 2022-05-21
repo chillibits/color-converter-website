7cf4ef74c371113f7c4f2718b0ccc0965bc28e8a
Marc Auberer <marc.auberer@chillibits.com>
2022-05-21 20:02:06 +0200

    Update stats

--- src/js/main.js
+++ src/js/main.js
@@ -413,7 +413,7 @@ $(window).scroll(function() {
 });
 
 function animateStats() {
-  animateValue("stats1", 46900);
+  animateValue("stats1", 47000);
   animateValue("stats2", 155);
   animateValue("stats3", 3500);
   animateValue("stats4", 3800);
